d5e3064a5322fdea356c0d1dc7395f8bd5c900e0
andrewmaltsev-v-v <68217531+andrewmaltsev-v-v@users.noreply.github.com>
2021-03-18 16:55:29 +0200

    Update first.js

--- JS homework 1/js 1st homework/first.js
+++ JS homework 1/js 1st homework/first.js
@@ -9,23 +9,23 @@
 let number = prompt ('vvdeite chislo ot 0 do 9', '');
 
 if (number == 1) {
-    console.log  ('odin');
+    console.log  ('one');
 } else if ( number == 2 ) {
-    console.log  ('dva');
+    console.log  ('two');
 } else if ( number == 3 ) {
-    console.log  ('3');
+    console.log  ('three');
 } else if ( number == 4 ) {
-    console.log  ('4');
+    console.log  ('four');
 } else if ( number == 5 ) {
-    console.log  ('5');
+    console.log  ('five');
 } else if ( number == 6 ) {
-    console.log  ('6');
+    console.log  ('six');
 } else if ( number == 7 ) {
-    console.log  ('7');
+    console.log  ('seven');
 } else if ( number == 8) {
-    console.log  ('8');
+    console.log  ('eight');
 } else if ( number == 9) {
-    console.log  ('9');
+    console.log  ('nine');
 } else {
     console.log  (' another number ');
 }
